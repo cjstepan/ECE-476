2d32d8d870a37f6f29a469019ddc74ccc5f72792
cjstepan <calvinstepan@gmail.com>
2024-10-13 20:56:46 -0500

    added 6.1-6.3

---
+++ homeworks/hw 6/6_2.py
@@ -0,0 +1,7 @@
+# 2) Write a Python program which uses edge interrupts to
+# -Increase a number by 1% each time you press GP15
+# -Decrese a number by 1% each time you press GP14
+# -Test your code with N starting at 1000
+
+
+
