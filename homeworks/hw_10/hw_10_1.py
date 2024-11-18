ac9c583f695fba474f49571a48ceccecdeabdabe
cjstepan <35180378+cjstepan@users.noreply.github.com>
2024-11-18 02:35:04 -0600

    Finished hw_10_1

--- homeworks/hw_10/hw_10_1.py
+++ homeworks/hw_10/hw_10_1.py
@@ -8,19 +8,23 @@ uart0.init(9600, bits=8, parity=None, stop=1, tx=0, rx=1) # tx/rx on pin 0/1
 
 # Button and Beeper Init
 Button15 = Pin(15, Pin.IN, Pin.PULL_UP)
+Button14 = Pin(14, Pin.IN, Pin.PULL_UP)
 Beeper = Pin(13, Pin.OUT)
 
 # LCD Init
 White = LCD.RGB(255,255,255)
 Black = LCD.RGB(0,0,0)
 Orange = LCD.RGB(255,140,0)
-Red = LCD.RGB(175,0,0)
+Red = LCD.RGB(222,0,0)
 
 LCD.Init()
 LCD.Clear(Black)
 
 # Global Flag
 Error_Flag = 0
+Record_Flag = 0
+speed = [0]*3
+knots_to_mph = 1.15078
 
 def Beep():
     Beeper.value(1)
@@ -79,10 +83,22 @@ def GPS_Read():
     
     return([time, lat, lon, speed])
 
-while(True):
-    speed = [0]*3
-    knots_to_mph = 1.15078
-    Record_Flag = 0
+def get_max_speed_mph(file, current_index):
+    f = open(file, "r")
+    data = f.readlines()
+    max_speed_mph = -1
+    for i in range(current_index, len(data)):
+        speed_mph = float(data[i])
+        if speed_mph > max_speed_mph:
+            max_speed_mph = speed_mph
+    return max_speed_mph
+
+LCD.Title('Top Speeds', White, Black)
+LCD.Text2('First Place: ', 40, 50, Orange, Black)
+LCD.Text2('Second Place: ', 40, 100, Orange, Black)
+LCD.Text2('Third Place: ', 40, 150, Orange, Black)
+
+while(Button14.value() == 1):
     Error_Flag = 1
     while(Error_Flag == 1):
         Error_Flag = 0
@@ -92,6 +108,7 @@ while(True):
         if(Record_Flag):
             Beep()
             f =  open("GPS_Speed.txt", "a")
+            current_index = len(f.readlines())
             print('Recording')
             LCD.Text('Recording',5,5,Red,Black)
         else:
@@ -101,18 +118,23 @@ while(True):
             f.close()
             print('Recording Stopped')
             LCD.Text('         ',5,5,Red,Black)
+            
+            # parse the file to get the max speed for the recording and display it on the LCD Screen
+            max_speed_mph = get_max_speed_mph("GPS_Speed.txt", current_index)
+            speed.append(max_speed_mph) # add the velocity in mph to top speeds
+            print(f"Max Speed = {max_speed_mph:.3f}")
+            speed.sort(reverse=True) # sort the list to get the top 3 speeds
+            print(f"List={speed[0]:.3f}, {speed[1]:.3f}, {speed[2]:.3f}")
+            sleep_ms(50)
+            LCD.Text2(f'{speed[0]:.3f} mph ', 320, 50, White, Black)
+            LCD.Text2(f'{speed[1]:.3f} mph ', 320, 100, White, Black)
+            LCD.Text2(f'{speed[2]:.3f} mph ', 320, 150, White, Black)
         while(Button15.value() == 0):
             pass
     if(Record_Flag):
         f.write(f'{(v * knots_to_mph):.3f}  \n')
 
-    speed.append(v * knots_to_mph) # add the velocity in mph to top speeds
-    speed.sort(reverse=True) # sort the list to get the top 3 speeds
-    LCD.Title('Top Speeds', White, Black)
-    LCD.Text2('First Place: ', 40, 50, Orange, Black)
-    LCD.Text2('Second Place: ', 40, 100, Orange, Black)
-    LCD.Text2('Third Place: ', 40, 150, Orange, Black)
-    sleep_ms(500)
-    LCD.Text2(f'{speed[0]:.3f} mph ', 320, 50, White, Black)
-    LCD.Text2(f'{speed[1]:.3f} mph ', 320, 100, White, Black)
-    LCD.Text2(f'{speed[2]:.3f} mph ', 320, 150, White, Black)
+for _ in range(0,3):
+    Beep()  
+    sleep_ms(100)
+print("stop")
